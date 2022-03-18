7fca68890be86902e96f3ff2638cafdf49abc1d7
Ananth Vankipuram <ultrasounder@ananths-air.lan>
2022-03-17 22:39:55 -0700

    adding the video streaming component

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const fs = require("fs"); //file system Node.js filesystem API
+
+const app = express(); // creates an instance of express
+
+const port = 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("Hello, World");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}!`);
+// });
