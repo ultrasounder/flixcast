73cbdb7296350966d3504162664c9174df224bd7
Ananth Vankipuram <ultrasounder@ananths-air.lan>
2022-03-17 23:28:01 -0700

    adding changes to the streaming server

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require("express");
+const fs = require("fs");
 
-const fs = require("fs"); //file system Node.js filesystem API
-
-const app = express(); // creates an instance of express
-
+const app = express();
 const port = 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello, World");
-// });
+//
+// Registers a HTTP GET route for video streaming.
+//
+// Original code for this:
+// https://medium.com/better-programming/video-stream-with-node-js-and-html5-320b3191a6b6
+//
+app.get("/video", (req, res) => {
+  //
+  // Original video from here:
+  // https://sample-videos.com
+  //
+  const path = "./videos/SampleVideo_1280x720_1mb.mp4";
+  fs.stat(path, (err, stats) => {
+    if (err) {
+      console.error("An error occurred ");
+      res.sendStatus(500);
+      return;
+    }
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}!`);
-// });
+    res.writeHead(200, {
+      "Content-Length": stats.size,
+      "Content-Type": "video/mp4",
+    });
+    fs.createReadStream(path).pipe(res);
+  });
+});
+
+//
+// Starts the HTTP server.
+//
+app.listen(port, () => {
+  console.log(
+    `Microservice listening on port ${port}, point your browser at http://localhost:3000/video`
+  );
+});
